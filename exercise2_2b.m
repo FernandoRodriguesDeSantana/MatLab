a3c722516241d291c53f214056ecb8f6eb8c1521
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2025-04-04 15:02:49 -0300

    Add files via upload

---
+++ exercise2_2b.m
@@ -0,0 +1,11 @@
+n = -2:10;
+x = [1:7,6:-1:1];
+[x21,n21] = sigfold(x,n);
+[x21,n21] = sigshift(x21,n21,3);
+[x22,n22] = sigshift(x,n,2);
+[x22,n22] = sigmult(x,n,x22,n22);
+[x2,n2] = sigadd(x21,n21,x22,n22);
+stem(n2,x2);
+title('Sequencia problema 2.2b');
+xlabel('n');
+ylabel('x2(n)');
